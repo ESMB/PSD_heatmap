ac96576651d174dc7d6a67eead8997847faf1b98
Mathew <Mathew@c005123.wlan.net.ed.ac.uk>
2021-09-15 13:23:38 +0100

    Added overlay to make white and grey.

--- Heatmap.py
+++ Heatmap.py
@@ -54,12 +54,33 @@ background=255
 
 brain=np.loadtxt(path_to_brain_image)
 
+brain_orig=np.copy(brain)
 
 
 
 
-# Set values
-brain[brain==background]=0
+# Set valueg
+brain=np.copy(brain_orig)
+brain_overlay=np.copy(brain)
+
+
+
+
+brain_overlay[brain_overlay==0]=np.nan
+brain_overlay[brain_overlay==background]=0
+brain_overlay[brain_overlay==olfactory]=np.nan
+brain_overlay[brain_overlay==hippocampus]=np.nan
+brain_overlay[brain_overlay==cerebellum]=np.nan
+brain_overlay[brain_overlay==subcortex]=np.nan
+brain_overlay[brain_overlay==regionc]=0.5
+brain_overlay[brain_overlay==isocortex]=np.nan
+
+
+
+
+
+brain[brain==background]=np.nan
+brain[brain==0]=0
 brain[brain==olfactory]=olfactory_val
 brain[brain==hippocampus]=hippocampus_val
 brain[brain==cerebellum]=cerebellum_val
@@ -68,12 +89,15 @@ brain[brain==regionc]=regionc_val
 brain[brain==isocortex]=isocortex_val
 
 
+
 # plt.figsize=(40,40)
 plt.imshow(brain,cmap='Blues',vmin=mini,vmax=maxi)
 plt.tick_params(left = False, right = False , labelleft = False,labelbottom = False, bottom = False)
 plt.axis('Off')
 plt.colorbar()
-plt.savefig(path_to_save+"Figure.pdf",dpi=600)
+plt.imshow(brain_overlay,cmap='Greys',vmin=0,vmax=1)
+# plt.imshow(brain_outline,cmap='Greys',vmin=0,vmax=1)
+plt.savefig(path_to_save+"Figure.pdf",dpi=1000)
 
 im = Image.fromarray(brain)
 im.save(path_to_save+'test.tif')
